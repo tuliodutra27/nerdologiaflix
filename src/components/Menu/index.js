f6eeaf39207b25d401a7c909d392d8f800956e8a
Tulio Marcos <tuliodutra27@gmail.com>
2020-07-27 13:57:07 -0300

    correção de bugs

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -8,7 +8,7 @@ function Menu() {
     return (
         <nav className="Menu">
             <a href="/">
-                <img className="logo" src={Logo} alt="Logo do site" />
+                <img className="Logo" src={Logo} alt="Logo do site" />
             </a>
 
             <Button as="a" className="ButtonLink" href="/">
