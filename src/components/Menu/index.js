f12b043677d95fb3d00f815a22d44bcf6723f120
Tulio Marcos <tuliodutra27@gmail.com>
2020-07-28 11:06:04 -0300

    adiciondos cadastros, correção de bugs

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import Logo from '../../assets/img/Logo.png';
+import {Link} from 'react-router-dom';
+import Logo from '../../assets/img/nerdologiaflix.png';
 import './Menu.css';
 //import ButtonLink from '../components/ButtonLink';
 import Button from '../Button';
@@ -7,11 +8,11 @@ import Button from '../Button';
 function Menu() {
     return (
         <nav className="Menu">
-            <a href="/">
+            <Link to="/">
                 <img className="Logo" src={Logo} alt="Logo do site" />
-            </a>
+            </Link>
 
-            <Button as="a" className="ButtonLink" href="/">
+            <Button as={Link} className="ButtonLink" to="/cadastro/video">
                 Novo vídeo
             </Button>
         </nav>
--- src/index.js
+++ src/index.js
@@ -1,11 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Home from './pages/home';
+import CadastroVideo from './pages/cadastro/video'
+import CadastroCategoria from './pages/cadastro/categoria';
+
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
+
+function Erro(){
+    return (
+        <div>
+            <h1>Erro 404</h1>
+            <p>Página não encontrada</p>
+            <p>Clique <a href="/">aqui</a> para voltar à página inicial.</p>
+        </div>
+    )
+}
 
 ReactDOM.render( 
-<React.StrictMode >
-    <App / >
-    </React.StrictMode>,
+    <BrowserRouter>
+        <Switch>
+            <Route path="/" component={Home} exact />
+            <Route path="/cadastro/video" component={CadastroVideo} />
+            <Route path="/cadastro/categoria" component={CadastroCategoria} />
+            <Route component={Erro} />
+        </Switch>
+        
+    </BrowserRouter>,
     document.getElementById('root')
 );
