1e227cf20790e74f314db94fe0a8009c0e332957
Tulio Marcos <tuliodutra27@gmail.com>
2020-07-28 11:21:58 -0300

    correção de bug do carousel

--- src/components/Carousel/index.js
+++ src/components/Carousel/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import { VideoCardGroupContainer, VideoCardList as Title, ExtraLink } from './styles';
+import { VideoCardGroupContainer, Title, ExtraLink } from './styles';
 import VideoCard from './components/VideoCard';
-import Slider, {SliderItem} from './components/Slider';
+import Slider, { SliderItem } from './components/Slider';
 
 function Carousel({
   ignoreFirstVideo,
