0e19475dee8f6de59ee26afdd658d200db614020
Tulio Marcos <tuliodutra27@gmail.com>
2020-07-28 13:31:01 -0300

    adicionado formulário de cadastro básico

--- src/pages/cadastro/categoria/index.js
+++ src/pages/cadastro/categoria/index.js
@@ -6,6 +6,14 @@ function CadastroCategoria(){
     return (
         <PageDefault>
             <h1>Cadastro de Categoria</h1>
+
+            <form>
+                <label>Nome da categoria:
+                    <input type="text"/>
+                </label>
+                <button>Cadastrar</button>
+            </form>
+
             <Link to="/cadastro/video">
                 Cadastro de video
             </Link>
